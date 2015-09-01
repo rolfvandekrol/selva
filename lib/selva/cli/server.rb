7ff747a5a91cbdd1d9bce7fdc6bfd3b9f7950950
Rolf van de Krol <info@rolfvandekrol.nl>
2015-09-01 21:18:25 +0200

    Run the webpack watcher while running the server

--- lib/selva/cli/server.rb
+++ lib/selva/cli/server.rb
@@ -4,12 +4,16 @@
 Selva::CLI.class_eval do
   desc "server", "Run the server"
   def server
-    # Make sure the Puma Rack handler is available
-    require 'rack/handler/puma'
-
     # Figure out the root path of the application. The server uses this to 
     # set the log location.
     root = 4.times.reduce(__FILE__) {|x,| File.dirname(x)}
+
+    # Run the webpack watcher while running the server, so the assets gets
+    # updated.
+    Process.spawn './node_modules/.bin/webpack -w', chdir: root
+
+    # Make sure the Puma Rack handler is available
+    require 'rack/handler/puma'
 
     # Initalize the server rack app
     server = Selva::Server.new(
