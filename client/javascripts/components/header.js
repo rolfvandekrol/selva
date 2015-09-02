57de7726d13efbfc398bab48e883b332d793a202
Rolf van de Krol <info@rolfvandekrol.nl>
2015-09-02 09:55:25 +0200

    Add empty button to header search

--- client/javascripts/components/header.js
+++ client/javascripts/components/header.js
@@ -5,15 +5,37 @@ var Icon = require('./icon.js');
 
 var Search = React.createClass({
   displayName: 'HeaderSearch',
+
+  getInitialState: function() {
+    return {text: ''};
+  },
   render: function() {
+    var closeButton = [];
+    if (this.state.text != '') {
+      closeButton = (
+        <button className="header-search-empty" onClick={this.handleEmptyClick}><Icon icon="close" /></button>
+      );
+    }
     return (
       <div className="header-search">
         <form>
-          <button class="header-search-active"><Icon icon="search" /></button>
-          <input type="textfield" placeholder="Zoeken" />
+          <button className="header-search-active" onClick={this.handleActivateClick}><Icon icon="search" /></button>
+          <input type="textfield" placeholder="Zoeken" ref="searchField" onChange={this.handleTextChange} value={this.state.text} />
+          {closeButton}
         </form>
       </div>
     );
+  },
+  handleTextChange: function(event, value) {
+    this.setState({text: event.target.value});
+  },
+  handleActivateClick: function(e) {
+    e.preventDefault();
+    React.findDOMNode(this.refs.searchField).focus();
+  },
+  handleEmptyClick: function(e) {
+    e.preventDefault();
+    this.setState({text: ''});
   }
 });
 
