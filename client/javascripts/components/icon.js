58c5b39e20971c70b16b45676df3a0faf2887e8f
Rolf van de Krol <info@rolfvandekrol.nl>
2015-09-03 09:29:30 +0200

    Mark icon property of Icon as required

--- client/javascripts/components/icon.js
+++ client/javascripts/components/icon.js
@@ -3,6 +3,11 @@ var React = require('react');
 
 module.exports = React.createClass({
   displayName: 'Icon',
+
+  propTypes: {
+    icon: React.PropTypes.string.isRequired
+  },
+
   render: function() {
     return (
       <i className={"fa fa-" + this.props.icon}></i>
