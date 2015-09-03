3625716eea79427cfb42df29d8fb7fff3eed6502
Rolf van de Krol <info@rolfvandekrol.nl>
2015-09-03 09:30:14 +0200

    Add the collapse button

--- client/javascripts/components/sidebar.js
+++ client/javascripts/components/sidebar.js
@@ -3,12 +3,49 @@ var React = require('react');
 
 var Icon = require('./icon.js');
 var Logo = require('./logo.js');
+var SidebarCollapseStore = require('../stores/sidebar_collapse_store.js');
+
+var getStateFromStore = function() {
+  return {
+    state: SidebarCollapseStore.state()
+  };
+};
+
+var setBodyAttribute = function(state) {
+  document.body.setAttribute('data-sidebar-state', state);
+}
+
+var CollapseButton = React.createClass({
+  displayName: 'SidebarCollapseButton',
+
+  render: function() {
+    return (
+      <div className="sidebar-collapse-button">
+        <button onClick={this.handleClick}><Icon icon="caret-left" /></button>
+      </div>
+    )
+  },
+
+  handleClick: function(e) {
+    e.preventDefault();
+  }
+});
 
 module.exports = React.createClass({
   displayName: 'Sidebar',
+
+  getInitialState: function() {
+    return getStateFromStore();
+  },
+
+  componentDidMount: function() {
+    setBodyAttribute(this.state.state);
+  },
+
   render: function() {
     return (
       <div className="sidebar">
+        <CollapseButton />
         <Logo />
       </div>
     );
