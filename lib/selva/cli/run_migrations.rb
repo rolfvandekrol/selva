e46a970203ca799566399185b1cebb4dbb5e5a74
Rolf van de Krol <info@rolfvandekrol.nl>
2015-09-21 22:34:44 +0200

    Remove old ruby code

--- lib/selva/cli/run_migrations.rb
+++
@@ -1,9 +0,0 @@
-# CLI command to run the database migrations. Currently this is not used, 
-# because we didn't even implement the database connection yet.
-
-Selva::CLI.class_eval do
-  desc "run_migrations", "Run the RethinkDB migrations"
-  def run_migrations
-    
-  end
-end
